d74d28ba90ccef123a0be447ffb2df8171a8be7b
31701424 计算机1703 王志伟 <1344966591@qq.com>
2020-01-01 12:16:56 +0800

    test ++、--、+=、-=、*=、/=、%=

---
+++ ++--.c
@@ -0,0 +1,9 @@
+void main(){
+	int i;
+	i = 2;
+	print i;
+	++i;
+	print i;
+	--i;
+	print i;
+}
---
+++ +=-=.c
@@ -0,0 +1,16 @@
+void main(){
+	int i;
+	i = 2;
+	i += i+1;
+	print i;
+	i -= 1;
+	print i;
+	int j;
+	j = 2;
+	i *= j*2;
+	print i;
+	i /= j;
+	print i;
+	i %= i;
+	print i;
+}
